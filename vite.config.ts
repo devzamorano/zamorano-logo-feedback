6cad5204824b9d94c330ce2280e816cea8a339b8
devzamorano <web.serv.zamorano@gmail.com>
2026-07-22 07:32:47 -0600

    chore: fix dev server port to 5556

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: process.env.WEB_PORT ? Number(process.env.WEB_PORT) : 6667,
+    port: process.env.WEB_PORT ? Number(process.env.WEB_PORT) : 5556,
     strictPort: true,
     proxy: {
       '/api': `http://localhost:${process.env.PORT || 3001}`,
